5a21467bc153f993137c0b7bd051a03ee5550644
Anthony Meza <abryantmeza@gmail.com>
2021-06-30 18:23:44 +0000

    Added the post command and got authorization code information

--- APISpotifyEx.py
+++ APISpotifyEx.py
@@ -2,19 +2,31 @@ import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = "2b1a105e0bf94d69924ed5789171693f"
-secret_id = "487346bb76a54e05b308947a10a96ebe"
+#Important constant values
+CLIENT_id = "2b1a105e0bf94d69924ed5789171693f"
+SECRET_id = "487346bb76a54e05b308947a10a96ebe"
+AUTH_URL = 'https://accounts.spotify.com/api/token'
 
+#Request for 
+response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_id,
+    'client_secret': SECRET_id,
+})
 
-
-
-
-
-text_input = input("Give text you would like to input: ")
-
-url = 'http://text-processing.com/api/sentiment/'
-myobj = {'text': text_input}
-
-response = requests.post(url, data = myobj)
-
+print(response)
 print(response.json())
+
+access_token = response.json()['access_token']
+print(access_token)
+
+#headers = {
+#    'Authorization': 'Bearer {token}'.format(token=access_token)
+#}
+
+#BASE_URL = 'https://api.spotify.com/v1/'
+#track_id = '6mFkJmJqdDVQ1REhVfGgd1'
+#r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+#r = r.json()
+#print(r)
+
