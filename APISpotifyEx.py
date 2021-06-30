c452e39af6c1803d64c3dc0ff6f02ba981bb729c
Anthony Meza <abryantmeza@gmail.com>
2021-06-30 19:21:52 +0000

    Implemented get function and managed to print out the names of the albums located in the playlist

--- APISpotifyEx.py
+++ APISpotifyEx.py
@@ -7,26 +7,37 @@ CLIENT_id = "2b1a105e0bf94d69924ed5789171693f"
 SECRET_id = "487346bb76a54e05b308947a10a96ebe"
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 
-#Request for 
+#Request for access token
 response = requests.post(AUTH_URL, {
     'grant_type': 'client_credentials',
     'client_id': CLIENT_id,
     'client_secret': SECRET_id,
 })
-
-print(response)
-print(response.json())
-
 access_token = response.json()['access_token']
-print(access_token)
 
-#headers = {
-#    'Authorization': 'Bearer {token}'.format(token=access_token)
-#}
 
-#BASE_URL = 'https://api.spotify.com/v1/'
-#track_id = '6mFkJmJqdDVQ1REhVfGgd1'
-#r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
-#r = r.json()
-#print(r)
 
+#Get an album from spotify
+BASE_url = 'https://api.spotify.com/v1/'
+playlist_id = '37i9dQZF1DXcBWIGoYBM5M' #Todays top hits 50
+
+#Header for authorization
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+playlist_res = requests.get(BASE_url + 'playlists/' + playlist_id, headers = headers )
+print(playlist_res)
+
+playlist = playlist_res.json()
+
+
+#Print out information about the playlist
+
+print("10 songs on the top hits:")
+print("-------------------------")
+count = 0;
+for track in playlist["tracks"]["items"]:
+    if (track["track"] != None):#nd (count < 10):
+        print(count,":", track["track"]["album"]["name"])
+    count += 1
